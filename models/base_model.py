10b527a2a13035288f57118a18350d5bf0e29b31
Adebayo Jubreel <whizkidjubrealguy@gmail.com>
2022-11-26 16:42:11 +0000

    worked on the dict public method

--- models/base_model.py
+++ models/base_model.py
@@ -11,17 +11,25 @@ class BaseModel:
     Class containing attributes and methods which serves as a base model for other classes
     """
     def __init__(self):
+        """ Public instance attributes"""
+
         self.id = str(uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def __str__(self):
-        return ("[{}} ({}) {}".format(self.class.name, self.id, self.__dict__))
+        """ A string representation of the base model"""
+
+        return ("[{}} ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
 
     def save(self):
+        """ Changing the base model object and updating it"""
+
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        for k, v in self.__dict__.items():
+        """ A dictionary containing attributes of the base model"""
 
-
+        self.created_at = self.created_at.isoformat()
+        self.updated_at = self.updated_at.isoformat()
+        return vars(self)
