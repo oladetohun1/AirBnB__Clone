25e6ebc014ebdfa9c9b1e2dc8a7df361e5e1ecd7
Adebayo Jubreel <whizkidjubrealguy@gmail.com>
2022-11-27 21:11:27 +0000

    imported required models

--- console.py
+++ console.py
@@ -5,7 +5,15 @@ The entry point of the command interpreter
 """
 
 import cmd
-
+from models import storage
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+import re
 
 class Hbnb(cmd.Cmd):
     """Command lines for the command interpreter"""
