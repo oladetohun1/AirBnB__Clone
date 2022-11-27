24737af6d1faf4fe752b83995313305004d85448
oladetohun1 <oladetohunoluwakorede@gmail.com>
2022-11-27 21:22:03 +0000

    solved all pcodestyle issues

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -6,14 +6,17 @@ import unittest
 from models.base_model import BaseModel
 from datetime import datetime
 
+
 class TestBaseModel(unittest.Testcase):
+
     """
     Test for BaseModel Class Methods
     """
     def test_str(self):
         """Test if ___str__ show the right output"""
         self.objct = BaseModel()
-        self.string = f'[{self.objct.__class__.name}] ({self.obj.id}) {self.obj.__dict__}'
+        self.string = f'[{self.objct.__class__.name}]
+        ({self.obj.id}) {self.obj.__dict__}'
         self.assertEqual(self.string, str(self.objct))
 
     def test_save(self):
